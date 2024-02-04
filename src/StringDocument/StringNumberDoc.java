9627a9f0e8a5451e6b8e24631ee1b11604e8659a
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-02-05 00:26:27 +0300

    task1

---
+++ src/StringDocument/StringNumberDoc.java
@@ -0,0 +1,29 @@
+package StringDocument;
+
+
+
+
+public class StringNumberDoc {
+    public static void PrintBlock(String numberDoc) {
+        System.out.println(numberDoc.substring(0, 4) + numberDoc.substring(9, 14));
+    }
+
+    public static void ChangeBlock(String numberDoc) {
+        numberDoc = numberDoc.replaceAll("[a-zA-Z]{4}", "****");
+        System.out.println(numberDoc);
+    }
+    public static void PrintOnlyLetters(String numberDoc){
+numberDoc=numberDoc
+        .replaceAll("[0-9-]+","/")
+        .toLowerCase();
+        System.out.println(numberDoc);
+    }
+    public static void PrintLettersWithBuilder(String numberDoc){
+        numberDoc=numberDoc
+                .replaceAll("[0-9 -]+", "/")
+                .toUpperCase();
+        StringBuilder stringBuilder = new StringBuilder("Letters: ").append(numberDoc);
+        System.out.println(stringBuilder);
+
+    }
+}
