3693d10ec7efcf1ba0b7479073906b4cfb6d8b27
Artem Bukharov <artem.bukharov.2018@gmail.com>
2024-02-05 00:29:26 +0300

    task1

--- src/StringDocument/StringNumberDoc.java
+++ src/StringDocument/StringNumberDoc.java
@@ -1,8 +1,6 @@
 package StringDocument;
 
 
-
-
 public class StringNumberDoc {
     public static void PrintBlock(String numberDoc) {
         System.out.println(numberDoc.substring(0, 4) + numberDoc.substring(9, 14));
@@ -12,14 +10,16 @@ public class StringNumberDoc {
         numberDoc = numberDoc.replaceAll("[a-zA-Z]{4}", "****");
         System.out.println(numberDoc);
     }
-    public static void PrintOnlyLetters(String numberDoc){
-numberDoc=numberDoc
-        .replaceAll("[0-9-]+","/")
-        .toLowerCase();
+
+    public static void PrintOnlyLetters(String numberDoc) {
+        numberDoc = numberDoc
+                .replaceAll("[0-9-]+", "/")
+                .toLowerCase();
         System.out.println(numberDoc);
     }
-    public static void PrintLettersWithBuilder(String numberDoc){
-        numberDoc=numberDoc
+
+    public static void PrintLettersWithBuilder(String numberDoc) {
+        numberDoc = numberDoc
                 .replaceAll("[0-9 -]+", "/")
                 .toUpperCase();
         StringBuilder stringBuilder = new StringBuilder("Letters: ").append(numberDoc);
